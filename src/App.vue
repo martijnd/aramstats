b3aab45c52f3324678a2c27e9a8cb5fb0e5591ae
martijnd <martijn.dorsman@gmail.com>
2019-01-02 00:14:57 +0100

    WIP CORS fix

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Dashboard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from "./components/Dashboard.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Dashboard
   }
-}
+};
 </script>
 
 <style>
+body {
+  background-color: black;
+  color: white;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>ARAMStats</h1>
+    <input
+      type="text"
+      @change="fetchData"
+      v-model="username"
+    >
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      API_KEY: process.env.VUE_APP_API_KEY,
+      API_URL:
+        "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/",
+      username: ""
+    };
+  },
+  methods: {
+    fetchData: function() {
+      const axios = require("axios");
+
+      axios
+        .get(this.API_URL + this.username + this.API_KEY)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+
+  mounted() {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h1 {
+  color: white;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
