e0526666049b79e78ff9b3786420f58d146fa2f4
martijnd <martijn.dorsman@gmail.com>
2019-01-06 12:27:15 +0100

    Parsed match data

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const teemoJS = require("teemojs");
 const api = teemoJS(process.env.API_KEY);
+const championData = require("./champion.json");
+
+let ChampionIds = [];
+Object.keys(championData.data)
+  .map(k => championData.data[k])
+  .map(el => (ChampionIds[el.key] = el.name));
 
 const getAccountId = async userName => {
   let data;
@@ -11,7 +17,7 @@ const getAccountId = async userName => {
   } catch (error) {
     console.error(error);
   }
-  console.log(data.name + "'s summoner id is " + data.accountId + ".");
+  console.log(data.name + "'s summoner id is " + data.id + ".");
 
   return data.accountId;
 };
@@ -22,18 +28,36 @@ const getMatchData = async (accountId, gameType = null) => {
     gameType = 430;
   }
   try {
-    return api.get("euw1", "match.getMatchlist", accountId, {
+    const data = await api.get("euw1", "match.getMatchlist", accountId, {
       queue: gameType
     });
+    return data.matches;
   } catch (error) {
     console.error(error);
   }
 };
 
-const parseMatchData = async matchData => {};
+const parseMatchData = async matchData => {
+  matchData = Object.keys(matchData)
+    .map(k => matchData[k])
+    .reverse();
+
+  let matchResult = [];
+
+  for (let property in matchData) {
+    const match = matchData[property];
+    match.timestamp = new Date(match.timestamp);
+    match.champion = ChampionIds[match.champion];
+
+    matchResult.push(match);
+  }
+
+  return matchResult;
+};
 
 (async () => {
-  const accountId = await getAccountId("Straenir");
+  const accountId = await getAccountId("gastlyguy");
   const matchData = await getMatchData(accountId, "ARAM");
   const result = await parseMatchData(matchData);
+  console.log(result);
 })();
