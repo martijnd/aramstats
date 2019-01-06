f8eb25f6d3da93c4128faac1c9499ba2dc596f4d
martijnd <martijn.dorsman@gmail.com>
2019-01-06 11:29:15 +0100

    Added match data and queue type getter

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 require("dotenv").config();
 const teemoJS = require("teemojs");
+const api = teemoJS(process.env.API_KEY);
 
-let api = teemoJS(process.env.API_KEY);
+const getAccountId = async userName => {
+  let data;
+  try {
+    data = await api
+      .get("euw1", "summoner.getBySummonerName", userName)
+      .catch(error => console.error(error));
+  } catch (error) {
+    console.error(error);
+  }
+  console.log(data.name + "'s summoner id is " + data.accountId + ".");
 
-let accountId = "";
-function getData() {
-  api.get("euw1", "summoner.getBySummonerName", "Gastlyguy").then(data => {
-    accountId = data.accountId;
-    // console.log(data);
-    console.log(data.name + "'s summoner id is " + data.accountId + ".");
-    getMatchData();
-  });
-}
-function getMatchData() {
-  console.log(accountId + " test");
-  api.get("euw1", "match.getMatchlist", accountId).then(response => {
-    console.log(response);
-  });
-}
+  return data.accountId;
+};
+const getMatchData = async (accountId, gameType = null) => {
+  if (gameType.toUpperCase() === "ARAM") {
+    gameType = 450;
+  } else if (gameType.toUpperCase() === "NORMAL") {
+    gameType = 430;
+  }
+  try {
+    return api.get("euw1", "match.getMatchlist", accountId, {
+      queue: gameType
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-getData();
+const parseMatchData = async matchData => {};
+
+(async () => {
+  const accountId = await getAccountId("Straenir");
+  const matchData = await getMatchData(accountId, "ARAM");
+  const result = await parseMatchData(matchData);
+})();
